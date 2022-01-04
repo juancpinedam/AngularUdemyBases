d6f80d37c4cf504978f85d138381f6e58f028be2
jpineda <jpineda@aceitesa.com>
2022-01-04 13:11:47 -0500

    primer

---
+++ src/app/contador.component.ts
@@ -0,0 +1,22 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+  selector: 'app-contador',
+  templateUrl: './contador.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class ContadorComponent{
+  title:string = 'Contador APP';
+  otracosa:string = 'Contador APP2';
+  numero:number = 0;
+  base:number = 5;
+
+
+  acumular(valor:number){
+    this.numero +=valor;
+  }
+
+
+}
